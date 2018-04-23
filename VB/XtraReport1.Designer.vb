340b7e2a3b059c5208dae3d6d917edbe4e588e94
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ VB/XtraReport1.Designer.vb
@@ -0,0 +1,75 @@
+<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Public Class XtraReport1
+    Inherits DevExpress.XtraReports.UI.XtraReport
+
+    'XtraReport overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        If disposing AndAlso components IsNot Nothing Then
+            components.Dispose()
+        End If
+        MyBase.Dispose(disposing)
+    End Sub
+
+    'Required by the Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Designer
+    'It can be modified using the Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Detail = New DevExpress.XtraReports.UI.DetailBand
+        Me.PageHeader = New DevExpress.XtraReports.UI.PageHeaderBand
+        Me.PageFooter = New DevExpress.XtraReports.UI.PageFooterBand
+        Me.NwindDataSet1 = New CustomDataItemsNames.nwindDataSet
+        Me.CategoriesTableAdapter = New CustomDataItemsNames.nwindDataSetTableAdapters.CategoriesTableAdapter
+        CType(Me.NwindDataSet1, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me, System.ComponentModel.ISupportInitialize).BeginInit()
+        '
+        'Detail
+        '
+        Me.Detail.Name = "Detail"
+        Me.Detail.Padding = New DevExpress.XtraPrinting.PaddingInfo(0, 0, 0, 0, 100.0!)
+        Me.Detail.TextAlignment = DevExpress.XtraPrinting.TextAlignment.TopLeft
+        '
+        'PageHeader
+        '
+        Me.PageHeader.Height = 30
+        Me.PageHeader.Name = "PageHeader"
+        Me.PageHeader.Padding = New DevExpress.XtraPrinting.PaddingInfo(0, 0, 0, 0, 100.0!)
+        Me.PageHeader.TextAlignment = DevExpress.XtraPrinting.TextAlignment.TopLeft
+        '
+        'PageFooter
+        '
+        Me.PageFooter.Height = 30
+        Me.PageFooter.Name = "PageFooter"
+        Me.PageFooter.Padding = New DevExpress.XtraPrinting.PaddingInfo(0, 0, 0, 0, 100.0!)
+        Me.PageFooter.TextAlignment = DevExpress.XtraPrinting.TextAlignment.TopLeft
+        '
+        'NwindDataSet1
+        '
+        Me.NwindDataSet1.DataSetName = "nwindDataSet"
+        Me.NwindDataSet1.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema
+        '
+        'CategoriesTableAdapter
+        '
+        Me.CategoriesTableAdapter.ClearBeforeFill = True
+        '
+        'XtraReport1
+        '
+        Me.Bands.AddRange(New DevExpress.XtraReports.UI.Band() {Me.Detail, Me.PageHeader, Me.PageFooter})
+        Me.DataAdapter = Me.CategoriesTableAdapter
+        Me.DataMember = "Categories"
+        Me.DataSource = Me.NwindDataSet1
+        Me.Version = "8.2"
+        CType(Me.NwindDataSet1, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me, System.ComponentModel.ISupportInitialize).EndInit()
+
+    End Sub
+    Friend WithEvents Detail As DevExpress.XtraReports.UI.DetailBand
+    Friend WithEvents PageHeader As DevExpress.XtraReports.UI.PageHeaderBand
+    Friend WithEvents PageFooter As DevExpress.XtraReports.UI.PageFooterBand
+    Friend WithEvents NwindDataSet1 As CustomDataItemsNames.nwindDataSet
+    Friend WithEvents CategoriesTableAdapter As CustomDataItemsNames.nwindDataSetTableAdapters.CategoriesTableAdapter
+End Class
---
+++ VB/nwindDataSet.vb
@@ -0,0 +1,53 @@
+﻿Imports System
+Imports DevExpress.Data
+' ...
+
+Partial Class nwindDataSet
+    Inherits Global.System.Data.DataSet
+    Implements IDisplayNameProvider
+
+    Public Function GetDataSourceDisplayName1() _
+    As String Implements DevExpress.Data.IDisplayNameProvider.GetDataSourceDisplayName
+        ' Substitute the default datasource display name
+        ' with a custom one.
+        Return "Northwind Traders"
+    End Function
+
+    Public Function GetFieldDisplayName(ByVal fieldAccessors() As String) _
+    As String Implements DevExpress.Data.IDisplayNameProvider.GetFieldDisplayName
+        ' Hide the data member if its name ends with 'ID'.
+        If fieldAccessors(0).EndsWith("ID") Then
+            Return Nothing
+        End If
+
+        ' Hide the 'Products' table, because its fields are accessible 
+        ' via the 'CategoriesProducts' table only.
+        If fieldAccessors(0).StartsWith("Products") Then
+            Return Nothing
+        End If
+
+        ' Get a field name form the data member's name. 
+        Dim fieldName As String = fieldAccessors((fieldAccessors.Length - 1))
+
+        ' Insert spaces between separate words of a field name.
+        Return ChangeNames(fieldName)
+    End Function
+
+    Public Function ChangeNames(ByVal name As String) As String
+        Dim result As String = String.Empty
+        Dim isPrevLow As Boolean = False
+
+        Dim symb As Char
+        For Each symb In name
+            ' Check if a character is of upper case. To avoid spaces inside abbreviations,
+            ' check if the previous character is of upper case, too.
+            If [Char].IsUpper(symb) And isPrevLow Then
+                result += " " + symb
+            Else
+                result += symb
+            End If
+            isPrevLow = [Char].IsLower(symb)
+        Next symb
+        Return result
+    End Function
+End Class
